5507aaa568c1c77f8e29270c8ecbd6de9a9d33ad
今福洸司 <77386640+tf-Imafuku@users.noreply.github.com>
2022-08-13 13:29:45 +0900

    Add files via upload

---
+++ terraformポートフォリオ/viriable.tf
@@ -0,0 +1,14 @@
+variable "project" {
+  default = "test"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "domain" {
+  default = "koji-satumaimo.net"
+}
+variable "MyIP" {
+  default=  " xxx.xxx.xxx.xxx/32 "
+}
