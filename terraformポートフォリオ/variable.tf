136cc6a82fc738de09d3c3506e77516a2116b906
今福洸司 <77386640+tf-Imafuku@users.noreply.github.com>
2022-08-15 10:33:59 +0900

    Add files via upload

---
+++ terraformポートフォリオ/variable.tf
@@ -0,0 +1,19 @@
+variable aws_access_key {}
+
+variable aws_secret_key {}
+
+variable "project" {
+  default = "test"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "domain" {
+  default = "koji-satumaimo.net"
+}
+variable "MyIP" {
+  default=  " xxx.xxx.xxx.xxx/32 "
+}
+
